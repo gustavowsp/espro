853d064acbba37ce3a2f07903f9c71266ea43c27
Gustavo <Gustavocampos50@proton.me>
2023-06-20 10:05:04 -0300

    asdasd

--- core/settings.py
+++ core/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'templates/staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
